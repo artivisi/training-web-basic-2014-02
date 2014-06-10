453a4b9aa56357b3fd086358bc904a8f79a0569a
Endy Muhardin <endy.muhardin@gmail.com>
2014-06-10 16:17:30 +0700

    implement relasi One to Many

--- sample-code/belajar-springmvc/src/main/java/com/artivisi/belajar/domain/User.java
+++ sample-code/belajar-springmvc/src/main/java/com/artivisi/belajar/domain/User.java
@@ -1,9 +1,13 @@
 package com.artivisi.belajar.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import org.hibernate.annotations.GenericGenerator;
@@ -25,6 +29,9 @@ public class User {
     
     @Column(nullable = false, unique = true)
     private String email;
+    
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<UserActivity> daftarActivity = new ArrayList<UserActivity>();
 
     public String getId() {
         return id;
