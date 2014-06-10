3733ff260b7556b9ae54ee37d7e6078b9368d95c
kyubix <kenny_sword_mage@yahoo.com>
2014-06-10 15:00:16 +0700

    Fix UserActivity

--- sample-code/belajar-springmvc/src/main/java/com/artivisi/belajar/domain/UserActivity.java
+++ sample-code/belajar-springmvc/src/main/java/com/artivisi/belajar/domain/UserActivity.java
@@ -2,17 +2,26 @@ package com.artivisi.belajar.domain;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
 
 @Entity @Table(name="user_activity")
 public class UserActivity {
 	
-	@Id
+	@Id @GeneratedValue(generator = "system-uuid")
+	@GenericGenerator(name = "system-uuid", strategy = "uuid2")
+	@Column(name= "activity_id")
+	private String activityId;
+	
 	@Column(nullable= false, name="user_id")
 	private String userId;
 	
 	@Column(nullable= false, name="user_activity")
 	private String userActivity;
+
+	
 	
 }
