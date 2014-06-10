d56a1044d2215d099f1adab84d7fb23a9c921651
Endy Muhardin <endy.muhardin@gmail.com>
2014-06-10 14:45:31 +0700

    placeholder supaya gak conflict

--- sample-code/belajar-springmvc/src/main/resources/import.sql
+++ sample-code/belajar-springmvc/src/main/resources/import.sql
@@ -1,3 +1,16 @@
+-- sample user 
 insert into m_user (id,username,fullname,password,email) values ('endy', 'endy', 'Endy Muhardin', '123', 'endy@artivisi.com');
 insert into m_user (id,username,fullname,password,email) values ('dadang', 'dadang', 'Dadang Iswan', '123', 'dadang@artivisi.com');
 insert into m_user (id,username,fullname,password,email) values ('adi', 'adi', 'Adi Sulistiono', '123', 'adi@artivisi.com');
+
+-- sample role
+
+-- sample menu
+
+-- sample news
+
+-- sample general info
+
+-- sample user activity
+
+-- sample user password
