6d52ee39d2a45ec7f1fdd687c81b26299e75acc2
Endy Muhardin <endy.muhardin@gmail.com>
2014-06-10 13:35:29 +0700

    ganti primary key menjadi UUID

--- sample-code/belajar-springmvc/src/main/java/com/artivisi/belajar/domain/User.java
+++ sample-code/belajar-springmvc/src/main/java/com/artivisi/belajar/domain/User.java
@@ -5,12 +5,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import org.hibernate.annotations.GenericGenerator;
 
 @Entity @Table(name="m_user")
 public class User {
     
-    @Id @GeneratedValue
-    private Long id;
+    @Id @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name = "system-uuid", strategy = "uuid2")
+    private String id;
     
     @Column(nullable = false, unique = true)
     private String username;
