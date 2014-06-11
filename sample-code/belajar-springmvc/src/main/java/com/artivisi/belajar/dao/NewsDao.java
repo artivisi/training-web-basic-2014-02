1970a96cb9dfb28ee12c56c8e9c0b3bc474ccce9
nubiex <ferry.end@gmail.com>
2014-06-11 12:05:55 +0700

    test and save

---
+++ sample-code/belajar-springmvc/src/main/java/com/artivisi/belajar/dao/NewsDao.java
@@ -0,0 +1,67 @@
+package com.artivisi.belajar.dao;
+
+import com.artivisi.belajar.domain.News;
+import com.artivisi.belajar.domain.User;
+
+import java.util.Date;
+import java.util.List;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class NewsDao {
+	 @Autowired
+	    private SessionFactory sessionFactory;
+	    
+	    public void save(News n){
+	        sessionFactory.getCurrentSession().saveOrUpdate(n);
+	    }
+	    
+	    public void delete(News n){
+	        sessionFactory.getCurrentSession().delete(n);
+	    }
+	    
+	    public News cariById(String NewsId){
+	        return (News) sessionFactory.getCurrentSession().get(News.class, NewsId);
+	    }
+	    
+	    public List<News> cariSemuaNews(Integer start, Integer rows){
+	        return sessionFactory.getCurrentSession()
+	                .createQuery("select n from News n order by n.newsCode")
+	                .setFirstResult(start)
+	                .setMaxResults(rows)
+	                .list();
+	    }
+	    
+	    public List<News> cariByCrUser(String username){
+	        return sessionFactory.getCurrentSession()
+	                .createQuery("select n from News n where n.newsCreateUser = :uname ")
+	                .setParameter("uname", username)
+	                .list();
+	    }
+	    
+	    public News cariByCode(String Code){
+	        return (News) sessionFactory.getCurrentSession()
+	                .createQuery("select n from News n where n.newsCode = :code ")
+	                .setParameter("code", Code)
+	                .uniqueResult();
+	    }
+	    
+	    
+	    public List<News> cariByDate(Date Tgl){
+	        return sessionFactory.getCurrentSession()
+	                .createQuery("select n from News n where n.newsDate = :tgl order by n.newsDate")
+	                .setParameter("tgl", Tgl)
+	                .list();
+	    }
+	    
+	    public List<News> cariByDesc(String Desc){
+	        return sessionFactory.getCurrentSession()
+	                .createQuery("select n from News n where lower(n.newsDesc) like :desc order by n.newsDesc")
+	                .setParameter("desc", "%"+Desc.toLowerCase()+"%")
+	                .list();
+	    } 
+	    
+	    
+}
--- sample-code/belajar-springmvc/src/main/resources/import.sql
+++ sample-code/belajar-springmvc/src/main/resources/import.sql
@@ -11,7 +11,11 @@ insert into m_user_password(user_id,user_password) values ('adi', '123');
 -- sample menu
 
 -- sample news
-
+insert into m_news (newsId,news_code,news_createdate,news_createuser,news_date,news_desc) values ('1', '001', '2014-06-11', 'FERRY','2014-06-09','TRAINING DAYS 1 CUI');
+insert into m_news (newsId,news_code,news_createdate,news_createuser,news_date,news_desc) values ('2', '002', '2014-06-11', 'DANNY','2014-06-10','TRAINING DAYS 2 CUI');
+insert into m_news (newsId,news_code,news_createdate,news_createuser,news_date,news_desc) values ('3', '003', '2014-06-11', 'IMMAN','2014-06-11','TRAINING DAYS 3 CUI');
+insert into m_news (newsId,news_code,news_createdate,news_createuser,news_date,news_desc) values ('4', '004', '2014-06-11', 'KENNY','2014-06-12','TRAINING DAYS 4 CUI');
+insert into m_news (newsId,news_code,news_createdate,news_createuser,news_date,news_desc) values ('5', '005', '2014-06-11', 'SUGAN','2014-06-12','TRAINING DAYS 5 CUI');
 -- sample general info
 
 -- sample user activity
