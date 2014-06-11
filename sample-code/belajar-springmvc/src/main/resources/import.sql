015e59a0071ed82d7a356694246ef2a19dafc7b1
Endy Muhardin <endy.muhardin@gmail.com>
2014-06-11 13:49:20 +0700

    pindahkan sample data user password

--- sample-code/belajar-springmvc/src/main/resources/import.sql
+++ sample-code/belajar-springmvc/src/main/resources/import.sql
@@ -1,10 +1,7 @@
 -- sample user 
 insert into m_user (id,username,fullname,email) values ('endy', 'endy', 'Endy Muhardin', 'endy@artivisi.com');
-insert into m_user_password(user_id,user_password) values ('endy', '123');
 insert into m_user (id,username,fullname,email) values ('dadang', 'dadang', 'Dadang Iswan', 'dadang@artivisi.com');
-insert into m_user_password(user_id,user_password) values ('dadang', '123');
 insert into m_user (id,username,fullname,email) values ('adi', 'adi', 'Adi Sulistiono', 'adi@artivisi.com');
-insert into m_user_password(user_id,user_password) values ('adi', '123');
 
 -- sample role
 insert into m_roles(roles_id, role_name) values('r1','host');
@@ -36,3 +33,6 @@ insert into m_general_info (infoid,infoname,infolink) values ('adi', 'adi', 'sam
 -- sample user activity
 
 -- sample user password
+insert into m_user_password(user_id,user_password) values ('endy', '123');
+insert into m_user_password(user_id,user_password) values ('dadang', '123');
+insert into m_user_password(user_id,user_password) values ('adi', '123');
