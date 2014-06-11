6fedf9e599c13df41bd8aaa477984911c252130d
sugandhiwong <learning.coding2010@gmail.com>
2014-06-11 13:17:10 +0700

    UserPasswordDao & UserPassDaoTest

---
+++ sample-code/belajar-springmvc/src/test/java/com/artivisi/belajar/dao/UserPassDaoTest.java
@@ -0,0 +1,33 @@
+package com.artivisi.belajar.dao;
+import com.artivisi.belajar.domain.User;
+import com.artivisi.belajar.domain.UserPassword;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.artivisi.belajar.domain.User;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:aplikasi.xml")
+@Transactional(readOnly = true)
+public class UserPassDaoTest {
+	@Autowired private UserDao userDao;
+    @Autowired private UserPasswordDao userPasswordDao;
+    
+    @Test
+    public void testCariById(){
+        // cari user yang ada di sample data
+    	User u = userDao.cariById("endy");    	    	    	
+        UserPassword endy = userPasswordDao.cariByUser(u);        
+        Assert.assertNotNull(endy);       
+        
+        // cari user yang tidak ada
+        Assert.assertNull( userDao.cariById("123"));
+    }
+        
+}
