a86a2d2785fd8e5fd56c1f9cea0b48a556daec1b
kyubix <kenny_sword_mage@yahoo.com>
2014-06-11 13:27:19 +0700

    Add ActivityUserDAO

---
+++ sample-code/belajar-springmvc/src/main/java/com/artivisi/belajar/dao/UserActivityDao.java
@@ -0,0 +1,52 @@
+package com.artivisi.belajar.dao;
+
+import java.util.List;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.artivisi.belajar.domain.UserActivity;
+
+
+@Repository
+public class UserActivityDao {
+	@Autowired
+	private SessionFactory sessionFactory;
+	
+	public void save(UserActivity a){
+		sessionFactory.getCurrentSession().saveOrUpdate(a);
+	}
+	
+	public void delete(UserActivity a){
+		sessionFactory.getCurrentSession().delete(a);
+	}
+	
+	public UserActivity cariById(String id){
+		return (UserActivity) sessionFactory.getCurrentSession().get(UserActivity.class, id);
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<UserActivity> cariSemuaActivity(Integer start, Integer rows){
+		return sessionFactory.getCurrentSession()
+				.createQuery("select u from UserActivity u order by u.activity")
+				.setFirstResult(start)
+				.setMaxResults(rows)
+				.list();
+	}
+	
+	public UserActivity cariByUserActivity(String userActivity){
+		return (UserActivity) sessionFactory.getCurrentSession()
+				.createQuery("select u from UserActivity u where u.userActivity = :uactivity")
+				.setParameter("uactivity", userActivity)
+				.uniqueResult();
+	}
+	
+	public List<UserActivity> cariByUserId(String userId){
+		return sessionFactory.getCurrentSession()
+				.createQuery("select u from UserActivity u where u.user = :user")
+				.setParameter("user", userId)
+				.list();
+	}
+	
+}
