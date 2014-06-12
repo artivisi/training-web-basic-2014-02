5cf5f434df0d3ee5fd45385167498ee4a9df8023
johnny.wijaya <johnny.wijaya@paninlife.co.id>
2014-06-12 15:23:22 +0700

    Add function List and Form (User menu)

--- sample-code/belajar-springmvc/src/main/java/com/artivisi/belajar/dao/UserDao.java
+++ sample-code/belajar-springmvc/src/main/java/com/artivisi/belajar/dao/UserDao.java
@@ -5,8 +5,10 @@ import java.util.List;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
+@Transactional
 public class UserDao {
     @Autowired
     private SessionFactory sessionFactory;
