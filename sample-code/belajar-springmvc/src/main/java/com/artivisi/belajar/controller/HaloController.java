55d4bc1ee190ef66223851ac2c54873d7a3bb698
Endy Muhardin <endy.muhardin@gmail.com>
2014-06-12 10:37:33 +0700

    test nesting folder

--- sample-code/belajar-springmvc/src/main/java/com/artivisi/belajar/controller/HaloController.java
+++ sample-code/belajar-springmvc/src/main/java/com/artivisi/belajar/controller/HaloController.java
@@ -2,7 +2,6 @@ package com.artivisi.belajar.controller;
 
 import java.util.Date;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
